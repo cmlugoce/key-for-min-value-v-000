5af4577807cd55e9206e06e1208f7c1de7c39fa4
Cristina Lugo <cmlugoce@syr.edu>
2018-06-13 15:08:52 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,12 +3,12 @@
 
 def key_for_min_value(name_hash)
   smallest_key = nil
-  smallest_value = nil
+  min_value = nil
   name_hash.each do | key, value |
       if name_hash == {}
         return nil
-      elsif smallest_value == nil || value < smallest_value
-        smallest_value = value
+      elsif min_value == nil || value < min_value
+        min_value = value
         smallest_key = key
       end
   end
