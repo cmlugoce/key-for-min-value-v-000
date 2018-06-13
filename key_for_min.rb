e59585d53844e8e5e614923e702ba2ba83e88d02
Cristina Lugo <cmlugoce@syr.edu>
2018-06-13 14:48:55 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,10 +5,11 @@ def key_for_min_value(name_hash)
 
  ikea = {:chair => 25, :table => 85, :mattress => 450}
  
+ min_value = 25
  if ikea == 0 
    nil 
  elsif ikea.each do |key, value|
-   key 
+   min_value 
   end
 end
 end
